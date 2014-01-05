49a38a1c5f90da7384fb4195cd046930793551d9
Alexander Aprelev <alexander@aprelev.com>
2014-01-05 11:20:59 -0800

    Rename oradart to oracledart

--- oracledart/sample_oracledart_extension.dart
+++ oracledart/sample_oracledart_extension.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library sample_synchronous_extension;
+library oracledart_synchronous_extension;
 
 import 'dart-ext:oradart';
 
