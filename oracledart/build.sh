c40a88f96913c2e709aee146ca72bdfa04f2b80f
Alexander Aprelev <alexander@aprelev.com>
2014-01-18 19:16:26 -0800

    For build on mac create symbolic links to Oracle dynamic libraries

--- oracledart/build.sh
+++ oracledart/build.sh
@@ -1,1 +1,5 @@
-ORACLE_OCI_HOME=~/Downloads/instantclient-sdk-macos.x32-11.2.0.3.0/sdk ORACLE_OCCI_LIB_HOME=~/Downloads/instantclient_11_2-2 xcodebuild -project oracledart.xcodeproj -configuration ReleaseIA32 SYMROOT=`pwd`/../dart/xcodebuild
+ORACLE_OCI_HOME=~/Downloads/instantclient-sdk-macos.x32-11.2.0.3.0/sdk
+ORACLE_OCCI_LIB_HOME=~/Downloads/instantclient_11_2-2
+ln $ORACLE_OCCI_LIB_HOME/libocci.dylib.11.1 $ORACLE_OCCI_LIB_HOME/libocci.dylib
+ln $ORACLE_OCCI_LIB_HOME/libclntsh.dylib.11.1 $ORACLE_OCCI_LIB_HOME/libclntsh.dylib
+xcodebuild -project oracledart.xcodeproj -configuration ReleaseIA32 SYMROOT=`pwd`/../dart/xcodebuild
