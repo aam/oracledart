65a6f2e8680e1a90a635e028fbbfd130f4370bc4
Alexander Aprelev <alexander@aprelev.com>
2014-03-03 17:22:21 -0500

    Fixed mac build/test

--- test/run-test-mac.sh
+++ test/run-test-mac.sh
@@ -1,2 +1,2 @@
-DYLD_LIBRARY_PATH=~/Downloads/instantclient_11_2-2 ../dart/xcodebuild/ReleaseIA32/dart 	test_oracledart_sync_extension.dart
+DYLD_LIBRARY_PATH=~/Downloads/instantclient_11_2-2 ../../dart/xcodebuild/ReleaseIA32/dart oracledart_sync_extension_test.dart
 
