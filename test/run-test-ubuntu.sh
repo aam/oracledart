abd8801f874d606f0b74ddfca4b847a1a95aa98a
Alexander Aprelev <alexander@aprelev.com>
2014-03-07 00:11:29 -0500

    Fix linux test script

--- test/run-test-ubuntu.sh
+++ test/run-test-ubuntu.sh
@@ -1,2 +1,3 @@
-LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 ../../dart/out/DebugIA32/dart oracledart_sync_extension_test.dart
+#!/bin/bash
+LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 ../../dart/out/ReleaseIA32/dart oracledart_sync_extension_test.dart
 
