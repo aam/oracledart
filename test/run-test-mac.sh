14da7315c246217025255a46f06deb5bfb807fea
Alexander Aprelev <aprelev@gmail.com>
2014-03-07 22:28:39 -0500

    Took dependency on dart-sdk out as it breaks on ubuntu

--- test/run-test-mac.sh
+++ test/run-test-mac.sh
@@ -1,2 +1,2 @@
-DYLD_LIBRARY_PATH=~/Downloads/instantclient_11_2-2 ../xcodebuild/ReleaseIA32/dart oracledart_sync_extension_test.dart
+DYLD_LIBRARY_PATH=~/Downloads/instantclient_11_2-2 ../../dart/xcodebuild/ReleaseIA32/dart oracledart_sync_extension_test.dart
 
