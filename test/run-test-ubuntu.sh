d8af2d30d34ad339c12b5bf56b6d803e230563ef
Alexander Aprelev <alexander@aprelev.com>
2014-03-07 20:53:15 -0500

    Fix path to dart in test scripts

--- test/run-test-ubuntu.sh
+++ test/run-test-ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 ../../dart/out/ReleaseIA32/dart oracledart_sync_extension_test.dart
+LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 ../out/ReleaseIA32/dart oracledart_sync_extension_test.dart
 
--- test/run-test.cmd
+++ test/run-test.cmd
@@ -1,3 +1,3 @@
 set path=%userprofile%\downloads\instantclient-basic-nt-12.1.0.1.0\instantclient_12_1\vc11;%userprofile%\downloads\instantclient-basic-nt-12.1.0.1.0\instantclient_12_1;%path%
 rem copy ..\\..\\dart\\build\\ReleaseIA32\\oracledart_extension.dll
-..\..\dart\build\ReleaseIA32\dart oracledart_sync_extension_test.dart
+..\build\ReleaseIA32\dart oracledart_sync_extension_test.dart
