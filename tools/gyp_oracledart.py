f454363ffdcf61ad72f43fe0099c02e65f13403b
Alexander Aprelev <alexander@aprelev.com>
2014-03-15 22:56:31 -0400

    Enabled rtti in addition to exceptions for Mac

--- tools/gyp_oracledart.py
+++ tools/gyp_oracledart.py
@@ -14,35 +14,32 @@ def execute(args):
   process.wait()
   return process.returncode
 
+def replace_in_config_file(filename_suffix, replacements):
+  infile = open('dart/tools/gyp/configurations_%s.gypi' % filename_suffix)
+  outfile = open('oracledart/tools/gyp/configurations_%s.gypi' % filename_suffix, 'w')
+  for line in infile:
+    for source, target in replacements.iteritems():
+      line = line.replace(source, target)
+    outfile.write(line)
+  infile.close()
+  outfile.close()
+
+
 def main():
   shutil.rmtree('oracledart/tools/gyp', ignore_errors=True)
   shutil.copytree('dart/tools/gyp',
       'oracledart/tools/gyp',
       ignore=shutil.ignore_patterns('.svn'))
-  infile = open('dart/tools/gyp/configurations_msvs.gypi')
-  outfile = open('oracledart/tools/gyp/configurations_msvs.gypi', 'w')
-  for line in infile:
-    line = line.replace("'ExceptionHandling': '0',", "'ExceptionHandling': '1',")
-    outfile.write(line)
-  infile.close()
-  outfile.close()
 
-  infile = open('dart/tools/gyp/configurations_make.gypi')
-  outfile = open('oracledart/tools/gyp/configurations_make.gypi', 'w')
-  for line in infile:
-    line = line.replace("'-fno-exceptions',", "'-fexceptions',")
-    outfile.write(line)
-  infile.close()
-  outfile.close()
-
-  infile = open('dart/tools/gyp/configurations_xcode.gypi')
-  outfile = open('oracledart/tools/gyp/configurations_xcode.gypi', 'w')
-  for line in infile:
-    line = line.replace("'GCC_ENABLE_CPP_EXCEPTIONS': 'NO', # -fno-exceptions", 
-        "'GCC_ENABLE_CPP_EXCEPTIONS': 'YES', # -fexceptions")
-    outfile.write(line)
-  infile.close()
-  outfile.close()
+  replace_in_config_file('msvs', 
+      {"'ExceptionHandling': '0',": "'ExceptionHandling': '1',"})
+  replace_in_config_file('make', 
+      {"'-fno-exceptions',": "'-fexceptions',"})
+  replace_in_config_file('xcode', 
+      {"'GCC_ENABLE_CPP_EXCEPTIONS': 'NO', # -fno-exceptions":
+          "'GCC_ENABLE_CPP_EXCEPTIONS': 'YES', # -fexceptions",
+       "'GCC_ENABLE_CPP_RTTI': 'NO', # -fno-rtti":
+          "'GCC_ENABLE_CPP_RTTI': 'YES', # -frtti"})
 
   args = ['python', 
           'dart/third_party/gyp/gyp_main.py',
