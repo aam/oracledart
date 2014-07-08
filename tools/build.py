493ae38556d48c775e3f975fd774fd732a14ede1
Alexander Aprelev <alexander@aprelev.com>
2014-07-07 21:45:44 -0400

    Added x64 Windows build

--- tools/build.py
+++ tools/build.py
@@ -5,23 +5,34 @@ import subprocess
 import os
 import sys
 import optparse
-import parser
+
 
 def Main():
+
     parser = optparse.OptionParser()
     parser.add_option("-p", "--publishToRepo",
-        help='Publish binaries to specified dist repo.',
-        default=None)
+                      help='Publish binaries to specified dist repo.',
+                      default=None)
+    parser.add_option("-a", "--architecture",
+                      help='Publish binaries to specified dist repo.',
+                      default='ia32')
     (options, args) = parser.parse_args()
 
     userhome = os.path.expanduser("~")
     platformid = platform.system()
     env = os.environ.copy()
+
+    targetExtensionLibrary = None
+
     if platformid == "Linux":
-        env["ORACLE_OCI_HOME"] = "%s/Downloads/instantclient_11_2/sdk" % userhome
-        env["ORACLE_OCCI_LIB_HOME"] = "%s/Downloads/instantclient_11_2" % userhome
+        env["ORACLE_OCI_HOME"] = (
+            "%s/Downloads/instantclient_11_2/sdk" % userhome)
+        env["ORACLE_OCCI_LIB_HOME"] = (
+            "%s/Downloads/instantclient_11_2" % userhome)
         buildCommand = ['make', '-j', '8', 'BUILDTYPE=ReleaseIA32']
-        extensionlibrary = "out/ReleaseIA32/lib.target/liboracledart_extension.so"
+        extensionlibrary = (
+            "out/ReleaseIA32/lib.target/liboracledart_extension.so")
+
     elif platformid == "Darwin":
         ORACLE_OCCI_LIB_HOME = "%s/Downloads/instantclient_11_2-2" % userhome
         subprocess.call(
@@ -30,46 +41,78 @@ def Main():
              "%s/libocci.dylib" % ORACLE_OCCI_LIB_HOME])
         subprocess.call(
             ["ln",
-             "%s/ORACLE_OCCI_LIB_HOME/libclntsh.dylib.11.1" % ORACLE_OCCI_LIB_HOME,
+             "%s/ORACLE_OCCI_LIB_HOME/libclntsh.dylib.11.1" %
+                ORACLE_OCCI_LIB_HOME,
              "%s/libclntsh.dylib" % ORACLE_OCCI_LIB_HOME])
-        env["ORACLE_OCI_HOME"] = "%s/Downloads/instantclient_11_2/sdk" % userhome
+        env["ORACLE_OCI_HOME"] = ("%s/Downloads/instantclient_11_2/sdk" %
+                                  userhome)
         env["ORACLE_OCCI_LIB_HOME"] = ORACLE_OCCI_LIB_HOME
         buildCommand = ['xcodebuild',
-                '-project', 'oracledart.xcodeproj',
-                '-configuration', 'ReleaseIA32',
-                'SYMROOT=%s/../dart/xcodebuild' % os.getcwd()]
+                        '-project', 'oracledart.xcodeproj',
+                        '-configuration', 'ReleaseIA32',
+                        'SYMROOT=%s/../dart/xcodebuild' % os.getcwd()]
         extensionlibrary = (
             "../dart/xcodebuild/ReleaseIA32/liboracledart_extension.dylib")
+
     elif platformid == "Windows" or platformid == "Microsoft":
-        if not "JAVA_HOME" in env:
-            print "JAVA_HOME is not set up"
+        if "JAVA_HOME" not in env:
+            print("JAVA_HOME is not set up")
             return 3
         os.system("mklink /J third_party ..\\dart\\third_party")
-        env["ORACLE_OCI_HOME"] = (
-            "%s\\downloads\\instantclient-sdk-nt-12.1.0.1.0\\instantclient_12_1\\sdk" %
-            userhome)
+        buildConfiguration = ""
+        if options.architecture == 'ia32':
+            env["ORACLE_OCI_HOME"] = (
+                "%s\\downloads\\"
+                "instantclient-sdk-nt-12.1.0.1.0\\instantclient_12_1\\sdk" %
+                userhome)
+            buildConfiguration = 'ReleaseIA32|Win32'
+        elif options.architecture == 'x64':
+            env["ORACLE_OCI_HOME"] = (
+                "%s\\downloads\\"
+                "instantclient-sdk-windows.x64-12.1.0.1.0\\"
+                "instantclient_12_1\\sdk" %
+                userhome)
+            buildConfiguration = 'ReleaseX64|x64'
+        else:
+            print("Invalid architecture specified %s" % (options.architecture))
+            return 4
+
         buildCommand = ['devenv.com',
-                'oracledart.sln',
-                '/build',
-                'ReleaseIA32']
+                        'oracledart.sln',
+                        '/build',
+                        buildConfiguration]
         extensionlibrary = (
-            "build\\ReleaseIA32\\oracledart_extension.dll")
+            "build\\%s\\oracledart_extension.dll" % (
+                "ReleaseX64" if options.architecture == 'x64'
+                else 'ReleaseIA32'))
+        if options.architecture == 'x64':
+            targetExtensionLibrary = "oracledart_extension_x64.dll"
     else:
-        print "Unsupported platform"
+        print("Unsupported platform")
         return 1
 
-    print " ".join(buildCommand)
+    if targetExtensionLibrary is None:
+        targetExtensionLibrary = ""
+
+    print(" ".join(buildCommand))
     process = subprocess.Popen(buildCommand, stdin=None, env=env)
     process.wait()
-    print "Completed with %d" % (process.returncode)
+    print("Completed with %d" % (process.returncode))
     if process.returncode == 0:
         if platformid == "Linux" or platformid == "Darwin":
-            subprocess.call("cp %s lib" % (extensionlibrary), shell=True)
-        else:
-            subprocess.call(["copy", extensionlibrary, "lib"], shell=True)
-        if (options.publishToRepo != None):
             subprocess.call(
-                ["python", "./tools/deploy.py", "-p", options.publishToRepo, "-f", extensionlibrary])
+                "cp %s lib/%s" % (extensionlibrary, targetExtensionLibrary),
+                shell=True)
+        else:
+            subprocess.call(
+                ["copy", extensionlibrary,
+                 "lib\\%s" % (targetExtensionLibrary)],
+                shell=True)
+        if (options.publishToRepo is not None):
+            subprocess.call(
+                ["python", "./tools/deploy.py",
+                 "-p", options.publishToRepo,
+                 "-f", extensionlibrary])
     return process.returncode
 
 if __name__ == '__main__':
