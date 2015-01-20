6d6ab1ef1229902f25724b39b4d2ef6c94550257
Alexander Aprelev <aam@chromium.org>
2015-01-19 19:34:37 -0800

    Fix x64 build script

--- test/run-test-x64.cmd
+++ test/run-test-x64.cmd
@@ -1,16 +1,16 @@
 set path=%userprofile%\Downloads\instantclient-basic-windows.x64-12.1.0.1.0\instantclient_12_1\vc11;%userprofile%\Downloads\instantclient-basic-windows.x64-12.1.0.1.0\instantclient_12_1;%path%
 
-copy packages\oracledart\oracledart_extension.dll packages\oracledart\oracledart_extension_ia32.dll
+copy packages\oracledart\src\oracledart_native_extension.dll packages\oracledart\src\oracledart_native_extension_ia32.dll
 
-copy packages\oracledart\oracledart_extension_x64.dll packages\oracledart\oracledart_extension.dll
+copy packages\oracledart\src\oracledart_native_extension_x64.dll packages\oracledart\src\oracledart_native_extension.dll
 
 ..\build\ReleaseX64\dart oracledart_test.dart
 set buildexitcode=%ERRORLEVEL%
 
-del packages\oracledart\oracledart_extension.dll
+del packages\oracledart\src\oracledart_native_extension.dll
 
-copy packages\oracledart\oracledart_extension_ia32.dll packages\oracledart\oracledart_extension.dll
+copy packages\oracledart\src\oracledart_native_extension_ia32.dll packages\oracledart\src\oracledart_native_extension.dll
 
-del packages\oracledart\oracledart_extension_ia32.dll
+del packages\oracledart\src\oracledart_native_extension_ia32.dll
 
 exit /b %buildexitcode%
